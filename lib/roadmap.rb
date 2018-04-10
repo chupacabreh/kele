f1b544d800dcc1fea317fd9bbffb99242c18da89
Matt Freitas <mattfreitas415@gmail.com>
2018-04-09 20:16:49 -0700

    messages complete

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -10,4 +10,20 @@ module Roadmap
     headers: { "authorization" => @auth_token })
     JSON.parse(response.body)
   end
+
+  def get_messages(page = 0)
+    if page > 0
+      request_url = "/message_threads?page=#{page}"
+    else
+      request_url = "/message_threads"
+    end
+    response = self.class.get(request_url,
+    headers: { "authorization" => @auth_token })
+    JSON.parse(response.body)
+  end
+
+  def create_message(user_id, recipient_id, subject, body)
+    self.class.post("/messages",
+    body: { user_id: user_id, recipient_id: recipient_id, subject: subject, stripped: body })
+  end
 end
