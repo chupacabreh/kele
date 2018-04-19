1f85e4d511fde16a26e4e9fb76870e6560496768
Matt Freitas <mattfreitas415@gmail.com>
2018-04-19 14:56:10 -0700

    cp 7 complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -8,8 +8,10 @@ class Kele
   base_uri 'https://www.bloc.io/api/v1'
 
   def initialize(email, password)
-    post_response = self.class.post('/sessions',
-    body: { email: email, password: password } )
+    post_response = self.class.post('/sessions', body: {
+      email: email,
+      password: password
+    } )
     @auth_token = post_response['auth_token']
     if @auth_token.nil?
       puts "Sorry, invalid credentials."
@@ -17,16 +19,17 @@ class Kele
   end
 
   def get_me
-    response = self.class.get('/users/me',
-    headers: { "authorization" => @auth_token })
-
+    response = self.class.get('/users/me', headers: { "authorization" => @auth_token })
     JSON.parse(response.body)
   end
 
   def get_mentor_availability(mentor_id)
-    response = self.class.get("/mentors/#{mentor_id}/student_availability",
-    headers: {"authorization" => @auth_token })
+    response = self.class.get("/mentors/#{mentor_id}/student_availability", headers: {"authorization" => @auth_token })
+    JSON.parse(response.body)
+  end
 
+  def get_remaining_checkpoints(chain_id)
+    response = self.class.get("/enrollment_chains/#{chain_id}/checkpoints_remaining_in_section", headers: { "authorization" => @auth_token })
     JSON.parse(response.body)
   end
 end
--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -1,13 +1,11 @@
 module Roadmap
   def get_roadmap(roadmap_id)
-    response = self.class.get("/roadmaps/#{roadmap_id}",
-    headers: { "authorization" => @auth_token })
+    response = self.class.get("/roadmaps/#{roadmap_id}", headers: { "authorization" => @auth_token })
     JSON.parse(response.body)
   end
 
   def get_checkpoint(checkpoint_id)
-    response = self.class.get("/checkpoints/#{checkpoint_id}",
-    headers: { "authorization" => @auth_token })
+    response = self.class.get("/checkpoints/#{checkpoint_id}", headers: { "authorization" => @auth_token })
     JSON.parse(response.body)
   end
 
@@ -17,13 +15,11 @@ module Roadmap
     else
       request_url = "/message_threads"
     end
-    response = self.class.get(request_url,
-    headers: { "authorization" => @auth_token })
+    response = self.class.get(request_url, headers: { "authorization" => @auth_token })
     JSON.parse(response.body)
   end
 
   def create_message(user_id, recipient_id, subject, body)
-    self.class.post("/messages",
-    body: { user_id: user_id, recipient_id: recipient_id, subject: subject, stripped: body })
+    self.class.post("/messages", body: { user_id: user_id, recipient_id: recipient_id, subject: subject, stripped: body })
   end
 end
